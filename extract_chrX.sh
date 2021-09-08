045168c668b6a15884a0c8d7e2c23012eb7058ce
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-09-08 13:23:25 -0700

    writing script to extract x-chromosome region of the 241-way WGA

--- extract_chrX.sh
+++ extract_chrX.sh
@@ -6,3 +6,6 @@ CHR_X_START="$(grep -n -m1 'Homo_sapiens.chr1' ~/alignments/sample.maf | cut -d 
 CHR_X_END="$(grep -n 'Homo_sapiens.chr1' ~/alignments/sample.maf | tail -1 | cut -d ':' -f1)"
 echo $CHR_X_START
 echo $CHR_X_END
+
+
+sed -n '$CHR_X_START,$CHR_X_END;' ~/alignments/sample.maf > extracted_region.maf
