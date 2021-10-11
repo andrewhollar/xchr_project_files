102a07e27eb84e9999c6c9b08cc48fb144a832ae
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-10-11 11:28:25 -0700

    added file to check lengths of BED entries

---
+++ get_bed_lengths.py
@@ -0,0 +1,15 @@
+import os, sys
+
+length_dict = {}
+
+with open(sys.argv[1], "r") as bed_in:
+    for line in bed_in.readlines():
+        line_tokens = line.split()
+        entry_length = int(line_tokens[2]) - int(line_tokens[1])
+
+        if entry_length in length_dict:
+            length_dict[entry_length] +=1
+        else:
+            length_dict[entry_length] = 1
+
+print(length_dict)
