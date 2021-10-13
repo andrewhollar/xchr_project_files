3807a55360cf55bd559801d6c0e6343941285c8d
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-10-12 18:30:17 -0700

    Added documentation to the get_bed_distribution script

--- extract_chrom_lengths.py
+++ extract_chrom_lengths.py
@@ -12,8 +12,6 @@ with open(sys.argv[1], "r") as chromInfo:
         line_tokens = line.split("|")
 
         if len(line_tokens) > 1:
-            # print(line_tokens)
-
             out_lines.append("{}\t{}\n".format(line_tokens[1].strip(), line_tokens[2].strip()))
 
 
--- get_bed_distribution.py
+++ get_bed_distribution.py
@@ -1,17 +1,37 @@
-import os, sys
+"""
+This script reads a BED file and counts the number of entries on each of the sequences included.
+The results are printed out to the console. 
+
+This script requires one input parameter -- the BED file to analyze.
+
+example:
+python get_bed_distribution.py /path/to/BED/file
+
+Output:
+chr 1   83575
+chr 2   77347
+...     ... 
+
+"""
+
+import sys
 
 contig_dict = {}
 
+# Open the BED file
 with open(sys.argv[1], "r") as bed_in:
+
+    # Read through BED file line by line. Each line in a BED file contains the entry for one genomic region.
     for line in bed_in.readlines():
         line_tokens = line.split()
-        # entry_length = int(line_tokens[2]) - int(line_tokens[1])
+
+        # Parse the sequence of the genomic region. This is the first column in the BED format.
         entry_contig = line_tokens[0]
         if entry_contig in contig_dict:
             contig_dict[entry_contig] +=1
         else:
             contig_dict[entry_contig] = 1
 
-
+# Print the counts 
 for key in sorted(contig_dict.keys()):
     print(key, contig_dict[key])
