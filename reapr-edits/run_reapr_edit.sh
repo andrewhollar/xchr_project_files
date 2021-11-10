f22c8c87577e41525ce3bd5a81272a26bc031599
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-11-09 17:24:45 -0800

    Fixed spelling

--- reapr-edits/run_reapr_edit.sh
+++ reapr-edits/run_reapr_edit.sh
@@ -4,7 +4,7 @@ OUTDIR=/home/ahollar/reapr_x
 
 #This is where the MAF preprocessing is done (Python 3)
 source ~/mafPreprocessingEnv/bin/activate
-python ~/xchr_project_files/reapr-edit/process_maf_file.py ~/alignments/6way_final.maf $OUTDIR
+python ~/xchr_project_files/reapr-edits/process_maf_file.py ~/alignments/6way_final.maf $OUTDIR
 deactivate
 
 #This is where REAPR is run (Python 2)
