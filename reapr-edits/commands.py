07f5bfc9803cefe4a6f0992824a31ab4865999bb
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-11-10 11:25:35 -0800

    Fixed path to the alistat utility

--- reapr-edits/commands.py
+++ reapr-edits/commands.py
@@ -1,5 +1,5 @@
 mlocarna='mlocarna'
 RNAz='RNAz'
 rnazWindow='/usr/local/share/RNAz/perl/rnazWindow.pl'
-alistat='/home/ahollar/software/AliStat/alistat'
+alistat='/home/ahollar/software/squid-1.9g/alistat'
 compalignp='compalignp'
