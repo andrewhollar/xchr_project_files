a7e8bae6e1c0007ef3bfd8b6d0f0d54b9132cba0
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-11-16 11:05:40 -0800

    Added file to bulk download the genomes in alignment. Added file containing the target accession numbers.

--- bulk_download_genomes.sh
+++ bulk_download_genomes.sh
@@ -5,12 +5,19 @@ cd ~/alignments
 mkdir 7way_genomes
 cd 7way_genomes
 
+GCA='GCA'
+GCF='GCF'
+
 cat ~/xchr_project_files/7way_accession_numbers.txt | while read -r asc ; do
 	esearch -db assembly -query $asc </dev/null \
 	| esummary \
 	| xtract -pattern DocumentSummary -element FtpPath_GenBank \
 	| while read  -r url ; do 
-		fname=$(echo $url | grep -o 'GCA_.*' | sed 's/$/_genomic.fna.gz/') ;
+        if [[ "$asc" == *"$GCF"* ]]; then
+            fname=$(echo $url | grep -o 'GCF_.*' | sed 's/$/_genomic.fna.gz/') ;
+        else
+		    fname=$(echo $url | grep -o 'GCA_.*' | sed 's/$/_genomic.fna.gz/') ;
+        fi
 		wget "$url/$fname" ;
 	done ;
 done
