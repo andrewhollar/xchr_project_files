b26feadfd5d7fd1ad9933c729300db68d91461a6
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-11-17 14:54:43 -0800

    Adding flanking procedure

--- reapr-edits/utilities.py
+++ reapr-edits/utilities.py
@@ -638,7 +638,7 @@ def get_flanked_sequence(species, contig, start, end, locus_bed_dir, locus_idx, 
     bed_error.close()
     
     flanked_seq = open(flanked_output).read()
-    print unflanked_seq.lower()
+    print (" " * 20) + unflanked_seq.lower()
     print flanked_seq.lower()
     assert unflanked_seq.lower() in flanked_seq.lower()
 
