e4ee5ac9aace426af2fe7241554e1732e178a55d
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-11-17 12:31:26 -0800

    Adding flanking procedure

--- reapr-edits/utilities.py
+++ reapr-edits/utilities.py
@@ -622,7 +622,7 @@ def get_flanked_sequence(species, contig, start, end, locus_bed_dir, locus_idx):
     
     cmd = '%s getfasta -fi %s -fo %s -bed %s' % (BEDTOOLS, species_to_genome_dict[species], flanked_output, bed_filepath)
     start_time = time.time()
-    subprocess.Popen(cmd, shell=True, stdout=subprocess.subprocess.STDOUT, stderr=subprocess.PIPE).wait()
+    subprocess.Popen(cmd, shell=True, stdout=subprocess.STDOUT, stderr=subprocess.PIPE).wait()
     print 'Running time: ' + str(time.time() - start_time) + ' seconds'
 
 # -------------------------------------------------------------------------------
