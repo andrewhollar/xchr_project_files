f2a95db471025914ea371a17983f91b83a08a9ec
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-11-25 16:45:59 -0800

    Added script to extract the BED coordinates of the reapr hits.

---
+++ extract_BED_from_reapr.py
@@ -0,0 +1,45 @@
+import os
+import sys 
+import argparse
+
+
+# Arguments: out BED file, Second RNAz score filter, Score difference,
+
+def main():
+    parser = argparse.ArgumentParser(description="Extract the BED locations of REAPR hits.")
+    parser.add_argument("-t", "--threshold", type=float, default=0.9, help="The cutoff score for the second RNAz pass (Default: 0.9).")
+    parser.add_argument("-d", "--difference", type=float, default=0.0, help="The allowed difference in RNAz scores (Default: 0).")
+    parser.add_argument("-o", "--output", required=True, help="The filepath of the output bed file containing the REAPR hits.")
+    parser.add_argument("-i", "--input_reapr_dir", required=True, help="The filepath to the directory containing REAPR output.")
+
+    args = parser.parse_args()
+    
+    reapr_summary_path = os.path.join(args.input_reapr_dir, "summary.tab")
+    assert os.path.isfile(reapr_summary_path)
+    assert os.stat(reapr_summary_path).st_size != 0
+    
+    header = True
+    header_line = ""
+    
+    # Column 0: Alignment block
+    # Column 4: RNAz score (first pass)
+    # Column 6: RNAz score (second pass)
+    
+    target_alignment_blocks = []
+    
+    with open(reapr_summary_path, "r") as summary_table:
+        for reapr_hit_line in summary_table.readlines():
+            if header:
+                header_line = reapr_hit_line
+                header = False
+            else:
+                line_tokens = reapr_hit_line.split()
+                rnaz_first = float(line_tokens[4])
+                rnaz_second = float(line_tokens[6])
+                
+                if rnaz_second > args.threshold:
+                    if rnaz_second - rnaz_first >= args.difference:
+                        target_alignment_blocks.append(line_tokens[0])
+                    
+                
+    print(target_alignment_blocks)
