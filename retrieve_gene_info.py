f68bb339cbb9789d97716877e41be83f8f0ace30
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-12-02 14:51:01 -0800

    Changed the gene name.

--- retrieve_gene_info.py
+++ retrieve_gene_info.py
@@ -2,21 +2,41 @@ import argparse
 import os
 from pyensembl import EnsemblRelease
 
+UCSC_BED_DIR = "/home/ahollar/bed_files/ucsc_hg38_gencode"
+UCSC_WHOLE_GENES = os.path.join(UCSC_BED_DIR, "ucsc_chrX_wholeGenes.bed")
+
+MAX_SAMPLES = 10
 
 def main():
     # release 77 uses human reference genome GRCh38
     data = EnsemblRelease(77)
 
-    #gene_info = data.gene_by_id("ENST00000431238")
-    gene_name = data.gene_name_of_transcript_id("ENST00000431238") 
-    gene_info = data.genes_by_name(gene_name)
-    print(gene_name)
-    print(gene_info)
+    # gene_name = data.gene_name_of_transcript_id("ENST00000431238") 
+    # gene_info = data.genes_by_name(gene_name)
+    # print(gene_name)
+    # print(gene_info)
+    
+    entries = 0
+    
+    for entry in open(UCSC_WHOLE_GENES, "r").readlines():
+        entry_tokens = entry.split()
+        start_pos = entry_tokens[1]
+        end_pos = entry_tokens[2]
+        transcript_id = entry_tokens[3]
+        
+        gene_name = data.gene_name_of_transcript_id(transcript_id.split('.'))
+        gene_info = data.genes_by_name(gene_name)
+
+        entries +=1
+        
+        if entries > MAX_SAMPLES:
+            break
+        
 
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    # parser = argparse.ArgumentParser()
     # parser.add_argument("-m", '--maf-file',
     #                     type=str, required=True,
     #                     help="input MAF file")
