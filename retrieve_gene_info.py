21854d9a66f76ac137911d151bbecfe2991194b8
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-12-02 14:14:47 -0800

    Adding script to get gene information from an ensemble id

--- retrieve_gene_info.py
+++ retrieve_gene_info.py
@@ -1,4 +1,32 @@
+import argparse
+import os
 from pyensembl import EnsemblRelease
 
-# release 77 uses human reference genome GRCh38
-data = EnsemblRelease(77)
+
+def main():
+    # release 77 uses human reference genome GRCh38
+    data = EnsemblRelease(77)
+
+    gene_info = data.gene_by_id("ENST00000431238.7")
+    print(gene_info)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("-m", '--maf-file',
+    #                     type=str, required=True,
+    #                     help="input MAF file")
+    # parser.add_argument("-r", '--reference-species',
+    #                     type=str,
+    #                     help="reference species, e.g., mm10, hg19")
+    # ARGS = parser.parse_args()
+    # MAF_FILEPATH = ARGS.maf_file
+    # REFERENCE_SPECIES = ARGS.reference_species
+    try:
+        main()
+        #run(MAF_FILEPATH, REFERENCE_SPECIES)
+    except FileNotFoundError:
+        # print("File {} not found".format(MAF_FILEPATH))
+        exit(1)
+    exit(0)
