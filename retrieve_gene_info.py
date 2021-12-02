9ab3369c78293573c6a2cb8c5db01022952d82a6
andrew hollar <andrewhollar@andrews-MBP.hitronhub.home>
2021-12-02 14:52:09 -0800

    Changed the gene name.

--- retrieve_gene_info.py
+++ retrieve_gene_info.py
@@ -24,9 +24,9 @@ def main():
         end_pos = entry_tokens[2]
         transcript_id = entry_tokens[3]
         
-        print(transcript_id)
+        # print(transcript_id)
         
-        gene_name = data.gene_name_of_transcript_id(transcript_id.split('.'))
+        gene_name = data.gene_name_of_transcript_id(transcript_id.split('.')[0])
         gene_info = data.genes_by_name(gene_name)
 
         entries +=1
